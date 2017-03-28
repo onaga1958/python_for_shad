6769cddc0b8417284a51b8527d71a1ef0bb00e5e
onaga1958 <alex--zabelin@mail.ru>
2017-03-28 04:29:49 +0300

    fix bug with ё

--- third/palindroms.py
+++ third/palindroms.py
@@ -3,15 +3,21 @@ import re
 
 def print_result(string):
     string = string.lower()
-    chars = [m.start() for m in re.finditer('[а-я|a-z]', string)]
+    chars = [m.start() for m in re.finditer('[а-яa-zё]', string)]
 
     for left, right in zip(chars, reversed(chars)):
         if left >= right:
             print('yes')
             return
-        if string[left] != string[right]:
+        if comp_char(string[left], string[right]):
             print('no')
             return
+
+def comp_char(c1, c2):
+    if c1 != 'е' and c1 != 'ё':
+        return c1 != c2
+    else:
+        return c2 != 'е' and c2 != 'ё'
 
 str_number = int(input())
 for i in range(str_number):
