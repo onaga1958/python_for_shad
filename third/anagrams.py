525f13237f39a5b3e431978bc2e3ab390d2884e8
onaga1958 <alex--zabelin@mail.ru>
2017-03-28 14:41:28 +0300

    keep trying to optimize anagrams

--- third/anagrams.py
+++ third/anagrams.py
@@ -1,38 +1,23 @@
-def get_letters_dict(word):
-    d = {}
-    for ch in word:
-        if d.get(ch) is None:
-            d[ch] = 1
-        else:
-            d[ch] += 1
-    return d
-
-
-def check_anagram(word1, word2):
-    dict1 = get_letters_dict(word1)
-    dict2 = get_letters_dict(word2)
-    return dict1 == dict2
+from collections import Counter
 
 number = int(input())
 
-anagrams = []
+anagrams = set()
+dict_anagram = {}
 
 for i in range(number):
     string = input().lower()
     for anagram in anagrams:
-        if check_anagram(string, anagram[0]):
-            anagram.append(string)
+        if Counter(string) == Counter(anagram):
+            dict_anagram[anagram].append(string)
             break
     else:
-        anagrams.append([string])
+        anagrams.add(string)
+        dict_anagram[string] = [string]
 
-result_strings = []
-for anagram in anagrams:
-    if len(anagram) > 1:
-        s = ""
-        for i, word in enumerate(sorted(anagram)):
-            s += word + (" " if i < len(anagram) - 1 else "")
-        result_strings.append(s)
-
-for s in sorted(result_strings):
-    print(s)
+for example in sorted(anagrams):
+    anagram_list = sorted(dict_anagram[example])
+    if len(anagram_list) > 1:
+        for i, word in enumerate(anagram_list):
+            print(word, end=' ' if i < len(anagram_list) else "")
+        print()
