7fa0c2da87ca4cd0a4aa31dbbcebddd17dca63d6
onaga1958 <alex--zabelin@mail.ru>
2017-04-02 14:46:57 +0300

    start work with date

--- third/date/date.py
+++ third/date/date.py
@@ -2,4 +2,8 @@ import re
 import sys
 
 queries = [line for line in sys.stdin]
-
+possible_forms = ['([0-9]{2}[-/.]){2}[0-9]{4}']
+for query in queries:
+    for form in possible_forms:
+        m = re.search(form, query)
+        if m.begin()
