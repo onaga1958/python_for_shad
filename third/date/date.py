7aa980a64f9f2383d159a1a0823b2d435d8a3f75
onaga1958 <alex--zabelin@mail.ru>
2017-04-02 17:17:50 +0300

    date wtih both variantes

--- third/date/date.py
+++ third/date/date.py
@@ -1,9 +1,30 @@
 import re
 import sys
 
-queries = [line for line in sys.stdin]
-possible_forms = ['([0-9]{2}[-/.]){2}[0-9]{4}']
+
+valid_date = (r"((^(([0-9]{2}\.){2}[0-9]{4}|([0-9]{2}-){2}[0-9]{4}|" +
+              "([0-9]{2}\/){2}[0-9]{4})$|^([0-9]{4}\.[0-9]{2}\.[0-9]" +
+              "{2}|[0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{4}\/[0-9]{2}\/" +
+              "[0-9]{2})$)|^[0-9]{1,2}\s*([а-я]+)\s*[0-9]{4}$)")
+
+
+delimiters = ['\.', '-', '\/']
+possible_forms = (['([0-9]{2}' +
+                   '{}'.format(d) +
+                   '){2}[0-9]{4}'
+                   for d in delimiters] +
+                  ['[0-9]{4}(' +
+                   '{}'.format(d) +
+                   '[0-9]{2}){2}'
+                   for d in delimiters] +
+                  ['[0-9]{1,2}\s*[а-яё]+\s*[0-9]{4}'])
+
+
+queries = [line.lower() for line in sys.stdin]
+
 for query in queries:
-    for form in possible_forms:
-        m = re.search(form, query)
-        if m.begin()
+    m = re.match(valid_date, query)
+    if m is not None:
+        print("YES")
+    else:
+        print("NO")
