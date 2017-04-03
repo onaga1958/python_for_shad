b0838debccf7f2b1999efc58d56c7d9867bfd546
onaga1958 <alex--zabelin@mail.ru>
2017-04-04 00:30:45 +0300

    start work with modules

---
+++ third/modules/modules.py
@@ -0,0 +1,19 @@
+import re
+import sys
+
+lines = [line.strip() for line in sys.stdin]
+
+modules = set()
+
+for line in lines:
+    matches = re.finditer("from +([a-zA-Z\.]+) +import", line)
+    for m in matches:
+        modules.add(m.group(1))
+        line = line.replace(m.group(0), '')
+
+    matches = re.finditer("import +([a-zA-Z\.]+(, +[a-zA-Z\.])*)", line)
+    for m in matches:
+        for module in m.group(1).split(" ,"):
+            modules.add(module)
+
+print(", ".join([s for s in sorted(modules)]))
